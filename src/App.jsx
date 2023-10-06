f93288e023ab440bfcda25cd0e5181edbff8505b
Adrian-YongKeng <141707001+Adrian-YongKeng@users.noreply.github.com>
2023-10-06 11:07:34 +0000

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,120 @@
+import './App.css'
+
+function Experience1 (){
+  return (
+    <>
+    <div className="experience">
+      <div className="experience-header">
+        <h3>Google, United States</h3>
+        <h3>Jan 2022 - Aug 2023</h3>
+      </div>
+        <h3 className="position">Front End Developer</h3>
+      </div>
+     <ul>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Autem, vel. Minus veniam, culpa perferendis architecto perspiciatis nesciunt voluptatibus reprehenderit consequatur quo neque asperiores voluptas tenetur velit praesentium deleniti cumque aperiam.</li>
+     </ul>
+    </>
+  )
+}
+
+function Experience2 (){
+  return (
+    <>
+    <div className="experience">
+      <div className="experience-header">
+        <h3>Google, United States</h3>
+        <h3>Jan 2020 - Oct 2021</h3>
+      </div>
+      <h3 className="position">Front End Developer</h3>
+    </div>
+     <ul>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum magnam quas veniam suscipit sed iste explicabo ducimus vel asperiores in error ex nulla, rerum earum sequi commodi ipsum! Repellendus, illum?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab ipsum sapiente facilis vel nobis maxime culpa voluptatibus tempora, non at?</li>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatem earum consequuntur nam, mollitia vero, deleniti beatae maxime doloremque tempore accusantium assumenda quae rem sit ducimus possimus aperiam repudiandae in non!</li>
+      <li>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex et excepturi ut sed minus maiores repellendus officiis, odio architecto est, consequuntur quaerat consectetur, necessitatibus molestiae explicabo debitis praesentium cumque impedit?</li>
+     </ul>
+    </>
+  )
+}
+
+function Experience3 (){
+  return (
+    <>
+    <div className="experience">
+      <div className="experience-header">
+        <h3>Google, United States</h3>
+        <h3>Oct 2017 - Dec 2019</h3>
+      </div> 
+        <h3 className="position">Front End Developer</h3>
+    </div>
+      <ul>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illo suscipit eligendi omnis fugiat dolorem ipsum quis impedit aut itaque quos dolore odit aliquid quidem, deserunt voluptas perspiciatis tempore ad?</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illo suscipit eligendi omnis fugiat dolorem ipsum quis impedit aut itaque quos dolore odit aliquid quidem, deserunt voluptas perspiciatis tempore ad?</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illo suscipit eligendi omnis fugiat dolorem ipsum quis impedit aut itaque quos dolore odit aliquid quidem, deserunt voluptas perspiciatis tempore ad?</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illo suscipit eligendi omnis fugiat dolorem ipsum quis impedit aut itaque quos dolore odit aliquid quidem, deserunt voluptas perspiciatis tempore ad?</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illo suscipit eligendi omnis fugiat dolorem ipsum quis impedit aut itaque quos dolore odit aliquid quidem, deserunt voluptas perspiciatis tempore ad?</li>
+        <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium illo suscipit eligendi omnis fugiat dolorem ipsum quis impedit aut itaque quos dolore odit aliquid quidem, deserunt voluptas perspiciatis tempore ad?</li>
+      </ul>
+    </>
+  )
+}
+
+function Academic (){
+  return(
+    <>
+     <div className="academic">
+      <h3>ACADEMIC CREDENTIALS</h3>
+      <ul>
+        <li><b>Aircraft Maintenance Engineering |</b><i>HAECO Aircraft Engineering, 2017</i></li>
+        <li><b>Software Developement Programme |</b><i>Sigma School, 2023</i></li>
+      </ul>
+        <p><b><i>Certificate :</i></b> EASA Part-147 Category B1.1 - Mechanical</p>
+        <p><b><i>Technical Skill :</i></b> JavaScript, CSS, HTML, Bootstrap, Tailwind CSS</p>
+      </div>
+    </>
+  )
+}
+
+function Language (){
+  return (
+    <>
+      <div className="personalDossier">
+       <h3>PERSONAL DOSSIER</h3>
+        <ul>
+          <li><b><i>Language Known :</i></b> English, Malay, Mandarin</li>
+        </ul>
+      </div>
+    </>
+  )
+}
+
+
+
+function App() {
+  return (
+    <div className="resume">
+      <div className="name">
+        <h1>Yeoh Yong Keng</h1>
+        <h2>Software Developer</h2>
+      </div>
+        <Experience1/>
+        <Experience2/>
+        <Experience3/>
+        <Academic/>
+        <Language/>
+    </div>
+  )
+}
+
+export default App
+
